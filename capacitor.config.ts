bef752a8c7a4d2908aa02eee97a50db248e71a00
mo1489h-eng <mo1489h@gmail.com>
2026-04-11 19:59:04 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,21 @@ const config: CapacitorConfig = {
   appId: 'com.coachbase.coachbase',
   appName: 'CoachBase',
   webDir: 'dist',
+  ios: {
+    contentInset: 'always',
+  },
+  android: {
+    backgroundColor: '#000000',
+  },
   plugins: {
     CapacitorUpdater: {
       appId: 'com.coachbase.coachbase'
-    }
+    },
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: '#000000',
+      showSpinner: false,
+    },
   }
 };
 
